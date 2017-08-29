6522a07074fff81d7afd97f4faf4e1f726c9571d
caixin426983 <836566274@qq.com>
2017-08-29 11:14:28 +0800

    测试，添加文件

---
+++ src/main/java/cx/Tools/Test.java
@@ -0,0 +1,13 @@
+package cx.Tools;
+
+/**
+ * Created by Administrator on 2017-8-29.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAaaa");
+    }
+
+
+}
